0323df76689b771fb2c20db62fd04a3f41dd5843
LimJiaEarn <119128612+LimJiaEarn@users.noreply.github.com>
2022-12-06 17:01:24 +0800

    Add files via upload

---
+++ PFC_CalendarInitialiser.py
@@ -0,0 +1,15 @@
+
+#Utility lists
+daysOfWeek = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+
+#RETURN_DICT Index List
+#Index 0 - YEAR
+#Index 1 - CalendarDaysAndMonths
+#Index 2 - START_DAY -> 0 is Monday, 6 is Sunday
+#Index 3 - left_headerTitles
+#Index 4 - right_headerTitles
+
+#This source code initialises the whole calendar itself
+
+def Initialise():
+    print("Success!")
