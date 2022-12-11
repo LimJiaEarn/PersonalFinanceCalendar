0480abc436653968bbbd557e3cc61e73d3b3c4c8
LimJiaEarn <119128612+LimJiaEarn@users.noreply.github.com>
2022-12-11 19:39:13 +0800

    Completed all major functions

--- PFC_UserBackend.py
+++ PFC_UserBackend.py
@@ -1,8 +1,8 @@
 # Contains all python code for UI
 
 #These are the default titles, there will be no change to this
-left_header_titles_original = {"Inflow":1, "Income":1, "Rent":1, "Subscriptions":1, "Comments":0}
-right_headerTitles_original = { }
+left_headerTitles_original = {"Inflow":1, "Income":1, "Comments":0}
+right_headerTitles_original = {"Rent":1, "Subscriptions":1, "Comments":0}
 week_titles_original = {"Date":0, "Notes":0, "Reflections":0, "Spent":1, "Comments":0}
 
 
@@ -17,8 +17,8 @@ week_titles_original = {"Date":0, "Notes":0, "Reflections":0, "Spent":1, "Commen
 RETURN_DICT = { "YEAR": 2023,\
                 "CalendarDaysAndMonths": {"January":31, "February":28, "March":31, "April":30},\
                 "START_DAY": 0,\
-                "left_headerTitles": {"Inflow":1, "Income":1, "Rent":1, "Subscriptions":1, "Comments":0},\
-                "right_headerTitles": {}, \
+                "left_headerTitles": {"Inflow":1, "Income":1, "Comments":0},\
+                "right_headerTitles": {"Rent":1, "Subscriptions":1, "Comments":0}, \
                 "week_titles": {"Date":0, "Notes":0, "Reflections":0, "Spent":1, "Comments":0}   }
 
 #For debugging
@@ -64,11 +64,10 @@ def UserSelection1Direct(selection1):
         setSTART_DAY()
 
     elif (selection1==2):
-        print("Edit left header")
+        EditHeaderTitles(3)
     
     elif (selection1==3):
-        print("Edit right header")
-
+        EditHeaderTitles(4)
     #Edit Weekly Rows
     elif (selection1==4):
         EditWeekTitles()
@@ -87,9 +86,9 @@ def UserMainSelection1():
         print("2 : Edit Left Header")
         print("3 : Edit Right Header")
         print("4 : Edit Weekly Rows")
-        print("0 : Proceed")
-
-        while True:
+        print("0 : Proceed!")
+        loop=True
+        while (loop):
             try:
                 selection1 = int(input("Selection: "))
                 if (selection1<0 or selection1 > 4):
@@ -99,6 +98,8 @@ def UserMainSelection1():
                         return False
                     else:
                         UserSelection1Direct(selection1)
+                        print("debugging: Exited UserSelection1Direct()")
+                        loop=False
 
             except ValueError:
                 print("Invalid selection! Use numbers")
@@ -145,9 +146,14 @@ def EditWeekTitles():
             
             #Adding week titles
             elif (choice==1):
-                newWeekTitle = input("Enter your new title here: ")
-                RETURN_DICT[list(RETURN_DICT)[5]][newWeekTitle] = 0 #Accounting value set to 0 as default
-                print("Successfully added title!")
+                while True:
+                    newWeekTitle = input("Enter your new title here: ")
+                    if (newWeekTitle in RETURN_DICT[list(RETURN_DICT)[5]] ):
+                        print("Title already exist!")
+                    else:
+                        RETURN_DICT[list(RETURN_DICT)[5]][newWeekTitle] = 0 #Accounting value set to 0 as default
+                        print("Successfully added title!")
+                        break
             
             #Removing week titles
             elif (choice==2):
@@ -164,7 +170,7 @@ def EditWeekTitles():
                         if (index<1 or index>len(RETURN_DICT[list(RETURN_DICT)[5]])-1):
                             print("You entered an invalid index")
                         else:
-                            del RETURN_DICT[list(RETURN_DICT)[5]][list(RETURN_DICT[list(RETURN_DICT)[5]])[4]]
+                            del RETURN_DICT[list(RETURN_DICT)[5]][list(RETURN_DICT[list(RETURN_DICT)[5]])[index]]
                             print("Successfully removed title!")
                             loop=False
                         
@@ -188,7 +194,15 @@ def EditWeekTitles():
                         else:
                             oldName=list(RETURN_DICT[list(RETURN_DICT)[5]])[index]  
                             newName = input("New week title name: ")
-                            list(RETURN_DICT[list(RETURN_DICT)[5]])[index]=newName
+                            temp_dict={}
+                            for key in RETURN_DICT[list(RETURN_DICT)[5]].keys():
+                                if (key != oldName):
+                                    temp_dict[key]=RETURN_DICT[list(RETURN_DICT)[5]][key]
+                                else:
+                                    temp_dict[newName]=RETURN_DICT[list(RETURN_DICT)[5]][oldName]
+                            RETURN_DICT[list(RETURN_DICT)[5]] = temp_dict
+                            if (oldName=="Spent"):
+                                RETURN_DICT[list(RETURN_DICT)[5]][newName] = 0
                             print(f"Successfully renamed from {oldName} to {newName}")
                             loop=False
 
@@ -219,10 +233,6 @@ def EditWeekTitles():
 def reOrder(Dictionary, startIndex):
     temp_dictionary={}
     check = [] #To check and ensure user do not enter repeated indexes
-    
-    #This loop is mainly for week titles since Date remains as index 0
-#     for i in range(startIndex):
-#         temp_dictionary[list(Dictionary)[i]] = Dictionary[list(Dictionary)[i]]
     
     length = len(Dictionary)
     for i in range(startIndex, length):
@@ -258,82 +268,98 @@ def reOrder(Dictionary, startIndex):
     
     return Dictionary
 
-def EditHeaderTitles(header_titles):
+def EditHeaderTitles(dictIndex):
     while True:
         try:
             print("Enter\n1 - Add title\n2 - Remove title\n3 - Rename titles\n4 - Re-order titles\n5 - Revert to default headers\n0 - Save and exit")
             choice = int(input("Enter: "))
+
+            #Check for valid choice number
+            if (choice<0 or choice>5):
+                print("Error!")
+                continue
+            
+            #Adding header titles
+            elif (choice==1):
+                while True:
+                    newHeaderTitle = input("Enter your new title here: ")
+                    if (newHeaderTitle in RETURN_DICT[list(RETURN_DICT)[dictIndex]] ):
+                        print("Title already exist!")
+                    else:
+                        RETURN_DICT[list(RETURN_DICT)[dictIndex]][newHeaderTitle] = 0 #Accounting value set to 0 as default
+                        print("Successfully added title!")
+                        break
+            
+            #Removing header titles
+            elif (choice==2):
+                print("Current header titles")
+                for i in range(len(RETURN_DICT[list(RETURN_DICT)[dictIndex]])):
+                    print(f"Index {i+1}: {list(RETURN_DICT[list(RETURN_DICT)[dictIndex]])[i]}")
+                print("Enter index of header title to remove: ")
+                loop=True
+                while (loop):
+                    try:
+                        index=int(input("Index: "))
+                        index-=1
+                        if (index<0 or index>len(RETURN_DICT[list(RETURN_DICT)[dictIndex]])-1):
+                            print("You entered an invalid index")
+                        else:
+                            del RETURN_DICT[list(RETURN_DICT)[dictIndex]][list(RETURN_DICT[list(RETURN_DICT)[dictIndex]])[index]]
+                            print("Successfully removed title!")
+                            loop=False
+
+                    except ValueError:
+                        print("Invalid index, it should be a number!")
+
+                
+            #Renaming header titles
+            elif (choice==3):
+                print("Current header titles")
+                for i in range(len(RETURN_DICT[list(RETURN_DICT)[dictIndex]])):
+                    print(f"Index {i+1}: {list(RETURN_DICT[list(RETURN_DICT)[dictIndex]])[i]}")
+                print("Enter index of header title to rename: ")
+                loop=True
+                while (loop):
+                    try:
+                        index=int(input("Index: "))
+                        index-=1
+                        if (index<0 or index>len(RETURN_DICT[list(RETURN_DICT)[dictIndex]])-1):
+                            print("You entered an invalid index")
+                        else:
+                            oldName=list(RETURN_DICT[list(RETURN_DICT)[dictIndex]])[index]   
+                            newName = input("New header title name: ")
+                            temp_dict={}
+                            for key in RETURN_DICT[list(RETURN_DICT)[dictIndex]].keys():
+                                if (key != oldName):
+                                    temp_dict[key]=RETURN_DICT[list(RETURN_DICT)[dictIndex]][key]
+                                else:
+                                    temp_dict[newName]=RETURN_DICT[list(RETURN_DICT)[dictIndex]][oldName]
+                            RETURN_DICT[list(RETURN_DICT)[dictIndex]] = temp_dict
+                            print(f"Successfully renamed from {oldName} to {newName}")
+                            loop=False
+                    except ValueError:
+                        print("Invalid index, it should be a number!")
+                    
+                
+            #Reordering header titles
+            elif (choice==4):  
+                RETURN_DICT[list(RETURN_DICT)[dictIndex]] = reOrder(RETURN_DICT[list(RETURN_DICT)[dictIndex]], 0)
+
+            #Reverting header titles back to default
+            elif (choice==5):
+                if (dictIndex==3):
+                    RETURN_DICT[list(RETURN_DICT)[dictIndex]] = left_headerTitles_original
+                else:
+                    RETURN_DICT[list(RETURN_DICT)[dictIndex+1]] = right_headerTitles_original
+                print("Successfully reverted!")
+            
+            #Exit
+            else:
+                return
+
         except ValueError:
             print("Error!")
     
-        #Check for valid choice number
-        if (choice<0 or choice>5):
-            print("Error!")
-            continue
-        
-        #Adding header titles
-        elif (choice==1):
-            newHeaderTitle = input("Enter your new title here: ")
-            header_titles[newHeaderTitle]=0
-            print("Successfully added title!")
-        
-        #Removing header titles
-        elif (choice==2):
-            print("Current header titles")
-            for i in range(len(header_titles)):
-                print(f"Index {i+1}: {header_titles[i]}")
-            print("Enter index of header title to remove: ")
-            while True:
-                try:
-                    index=int(input("Index: "))
-                    index-=1
-                except ValueError:
-                    print("Invalid index, it should be a number!")
-                if (index<0 or index>len(header_titles)-1):
-                    print("You entered an invalid index")
-                else:
-                    break
-            #header_titles.pop(index) V1 Code
-            del header_titles[list(header_titles)[index]]
-            print("Successfully removed title!")
-            
-        #Renaming header titles
-        elif (choice==3):
-            print("Current header titles")
-            for i in range(len(header_titles)):
-                print(f"Index {i+1}: {header_titles[i]}")
-            print("Enter index of header title to rename: ")
-            while True:
-                try:
-                    index=int(input("Index: "))
-                    index-=1
-                except ValueError:
-                    print("Invalid index, it should be a number!")
-                if (index<0 or index>len(header_titles)-1):
-                    print("You entered an invalid index")
-                else:
-                    break
-            oldName=header_titles[index]    
-            newName = input("New header title name: ")
-            #header_titles[index]=newName V1 Code
-            header_titles[list(header_titles)[index]] = newName
-            print(f"Successfully renamed from {oldName} to {newName}")
-            
-        #Reordering header titles
-        elif (choice==4):  
-            print("This function is not made yet!")
-        #Reverting header titles back to default
-        elif (choice==5):
-            header_titles = header_titles_original
-            print("Successfully reverted!")
-        
-        #Exit
-        else:
-            return header_titles
-
-
-
-
 
 
 def printCalendarHeaderTitles(header_titles):
